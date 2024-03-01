8b95b3a4d59f119995a6ed8cf130294da6c4fe1d
Jordan Pinnick <geometryse@gmail.com>
2024-03-01 16:35:06 -0500

    Reinitialize tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess({})],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter()
 		// paths: {
 		// 	base: process.env.NODE_ENV === 'production' ? '/double-pendulum-web' : ''
 		// }
